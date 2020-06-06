6ce1e6f128cf15be6efd8dfc0dedee508351a913
Razi <razibhatti1@gmail.com>
2020-06-06 19:51:38 +0500

    Working code

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
-from werkzeug.utils import secure_filename
-from flask import Flask, render_template, request, send_file
 import os
-import sys
+
+from flask import Flask, render_template, request, send_file
+from werkzeug.utils import secure_filename
 
 from module import transform
 
@@ -11,21 +11,26 @@ os.makedirs(os.path.join(app.instance_path, 'uploads'), exist_ok=True)
 
 @app.route("/", methods=['POST'])
 def startProcess():
-    src = request.files['source']
-    trg = request.files['target']
-    params = []
-    params.append(request.args.get("x"))
-    params.append(request.args.get("y"))
-    params.append(request.args.get("z"))
+    try:
+        src = request.files['source']
+        trg = request.files['target']
+    except:
+        return "Either source or target is empty"
+    params = [request.args.get("x"), request.args.get("y"), request.args.get("z")]
+    plength = sum(1 for x in params if x is not None)
+    if src is None or trg is None or plength < 3:
+        return "Either source or target is empty or some parameter is empty"
 
     src.save(os.path.join(app.instance_path,
                           'uploads', secure_filename(src.filename)))
     trg.save(os.path.join(app.instance_path,
                           'uploads', secure_filename(trg.filename)))
-
-    morph = transform.ManipulateSelfie(src.filename, trg.filename, params)
-    timage = morph.apply_transformation()
-    return send_file(timage, mimetype='image/jpg')
+    try:
+        morph = transform.ManipulateSelfie(src.filename, trg.filename, params)
+        response = morph.apply_transformation()
+        return send_file(response, mimetype='image/jpg')
+    except:
+        return "Some error occurred while processing", 400
 
 
 @app.route("/", methods=['GET'])
@@ -33,7 +38,7 @@ def index():
     try:
         return render_template('index.html')
     except:
-        return ("Some error occurred while trying to process")
+        return "Some error occurred while trying to fetch data"
 
 
 @app.route("/examples", methods=['GET'])
@@ -41,7 +46,7 @@ def getExamples():
     try:
         return render_template('examples.html')
     except:
-        return ("Some error occurred while trying to fetch data")
+        return "Some error occurred while trying to fetch data"
 
 
 if __name__ == "__main__":
